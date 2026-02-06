65266ef93f6809404e28ab2ee863f17faedf50d0
iamjuandev <juanmiguelar09@gmail.com>
2026-02-06 13:15:27 -0600

    fix notfound

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,10 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-01-01',
 
+  nitro: {
+    preset: 'vercel',
+  },
+
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/google-fonts',
